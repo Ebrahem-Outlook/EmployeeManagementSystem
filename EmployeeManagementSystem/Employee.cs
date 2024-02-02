734fb55886e9929babfbacf0e18e40fc771665c7
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-02-03 00:08:04 +0200

    V_1

---
+++ EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public String? Department { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() { }
+
+        public Employee(int _Id, String _Name, String _Department, double _Salary)
+        {
+            this.Id = _Id; 
+            this.Name = _Name; 
+            this.Department = _Department; 
+            this.Salary = _Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Department: {Department}, Salary: {Salary}";
+        }
+    }
+}
---
+++ EmployeeManagementSystem/ManagementSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    internal class ManagementSystem
+    {
+        private List<Employee> employees;
+
+        public ManagementSystem()
+        {
+            employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public void AddEmployee(int _Id, String _Name, String _Department, double _Salary)
+        {
+            employees.Add(new Employee(_Id, _Name, _Department, _Salary));
+        }
+
+        public void DisplayEmployees()
+        {
+            Console.WriteLine("Employee List::");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        public void UpdateEmployee(int _Id, String _Name, String _Department, double _Salary)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.Id == _Id)
+                {
+                    employee.Name = _Name;
+                    employee.Department = _Department;
+                    employee.Salary = _Salary;
+                    Console.WriteLine($"Employee with ID {_Id} updated succeefully.");
+                    return;
+                }
+            }
+            Console.WriteLine($"Employee with ID {_Id} not found.");
+        }
+
+        public void RemoveEmployee(int _Id)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                if (employees[i].Id == _Id)
+                {
+                    employees.RemoveAt(i);
+                    Console.WriteLine($"Employee with ID {_Id} deleted.");
+                    return;
+                }
+            }
+            Console.WriteLine($"Employee with ID {_Id} id not found");
+        }
+
+    }
+}
